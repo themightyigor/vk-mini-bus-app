4159ebd0c4245c5982ad32fcf3b29860c1c99e0b
themightyigor <vincentmarnisi@gmail.com>
2019-09-08 21:00:30 +0300

    Updates

---
+++ precache-manifest.4d6dafd0d2e2ef58c66f6cbf09e3b61c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffc79062af110f803abb",
+    "url": "/vk-mini-bus-schedule/static/css/main.46cd731c.chunk.css"
+  },
+  {
+    "revision": "ffc79062af110f803abb",
+    "url": "/vk-mini-bus-schedule/static/js/main.e5cc0974.chunk.js"
+  },
+  {
+    "revision": "cb6ad27a3114677658e1",
+    "url": "/vk-mini-bus-schedule/static/js/runtime~main.f3ac1a7f.js"
+  },
+  {
+    "revision": "a3913ee3026dd46decca",
+    "url": "/vk-mini-bus-schedule/static/css/2.76d22207.chunk.css"
+  },
+  {
+    "revision": "a3913ee3026dd46decca",
+    "url": "/vk-mini-bus-schedule/static/js/2.89d69196.chunk.js"
+  },
+  {
+    "revision": "277c586722a43a9521366ee45108bc9c",
+    "url": "/vk-mini-bus-schedule/static/media/logo.277c5867.svg"
+  },
+  {
+    "revision": "7a7c06ad27e8ae0615e185f4ecdefe28",
+    "url": "/vk-mini-bus-schedule/static/media/persik.7a7c06ad.png"
+  },
+  {
+    "revision": "49c0f113033d145daf7ceff5cf42e175",
+    "url": "/vk-mini-bus-schedule/index.html"
+  }
+];
